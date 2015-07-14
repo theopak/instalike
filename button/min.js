0f2a9d9a4ee2a6cc9c4d0cc83b89fbafb72b83d7
Theodore X. Pak <tpak@cimpress.com>
2015-07-14 16:09:52 -0400

    Update 2015-07-14T20:09:51.498Z

--- button/min.js
+++ button/min.js
@@ -348,22 +348,29 @@ var params = (function () {
 }());
 
 /**
- * State is true or false.
+ * State 'didLike' is true or false. Local state of the like for this
+ * 'Thing' hash prevents additional likes from POST-ing to the API.
  */
-var setState = function(liked) {
-  var state = liked ? true : false;
+var setLocalState = function(didLike) {
+  var state = didLike ? true : false;
   localStorage.setItem('liked_' + hash, state);
-  icon.id = 'instalike--icon-liked';
+  icon.id = state ? 'instalike--icon-liked' : 'instalike--icon';
+  // console.log('setLocalState(): state %s, icon.id %s', state, icon.id);
   return state;
 };
 
 /**
- * Local count for the 'thing' at this hash.
+ * Integer count for the 'thing' at this hash. This local count enables the
+ * browser to render the last known count for this 'Thing' hash, until it
+ * is replaced by any API calls.
  */
-var setCount = function(i) {
+var setLocalCount = function(i) {
+  i = parseInt(i || 0);
   if (i < 1) {
+    count = i;
+    label.id = 'instalike--label-empty';
     return 0;
-  };
+  }
   localStorage.setItem('count_' + hash, i);
   counter.innerHTML = i;
   count = i;
@@ -372,7 +379,7 @@ var setCount = function(i) {
 };
 
 /**
- * Elements and state.
+ * Elements and state vars.
  */
 var button  = document.getElementById('instalike');
 var icon    = document.getElementById('instalike--icon');
@@ -382,42 +389,46 @@ var hash = (params.thing || '0000').toString();
 var count = 0;
 
 /**
- * Load state from localStorage.
+ * Use localStorage to load the state and cached like count so that they
+ * can be rendered on the page before API operations complete.
  */
-if(localStorage.getItem('liked_' + hash)) {
-  setState(localStorage.getItem('liked_' + hash));
+if(localStorage.getItem('liked_' + hash) === 'true') {
+  setLocalState(localStorage.getItem('liked_' + hash));
   if(count != 0) {
     label.id = 'instalike--label';
   }
 }
-// if(localStorage.getItem('count_' + hash)) {
-//   setCount(localStorage.getItem('count_' + hash));
-// }
+setLocalCount(localStorage.getItem('count_' + hash) || 0);
 
 /**
- * Load count from API.
+ * Update like count via the API.
  */
 fetch('/' + hash)
   .then(function(response) { return response.json(); })
-  .then(function(json)     { setCount(json['count']); })
+  .then(function(json)     { setLocalCount(json['count']); })
   .catch(function(e)       { console.log('parsing failed', e); });
 
 /**
  * Handle a 'like' button press:
- *   - Send like to API.
+ *   - Send like/unlike to API.
  *   - Update local count.
  *   - Update local state press.
  *   - Trigger animations.
  */
-var handleLike = function(el) {
-  icon.style.transform = 'scale(1.2)';
-  icon.style.color = 'tomato';
-  icon.id = 'instalike--icon-liked';
-  fetch('/' + hash, { method: 'post' });
-  setCount(++count);
-  setState(true);
-  setTimeout(function() {
-    icon.removeAttribute('style');
-  }, 150);
+var handleClick = function(el) {
+  if (localStorage.getItem('liked_' + hash) === 'true') {
+    setLocalState(false);
+    setLocalCount(count = count - 1);
+    fetch('/' + hash, { method: 'patch' });
+  } else {
+    icon.style.transform = 'scale(1.2)';
+    icon.style.color = 'tomato';
+    setLocalState(true);
+    setLocalCount(count = count + 1);
+    fetch('/' + hash, { method: 'post' });
+    setTimeout(function() {
+      icon.removeAttribute('style');
+    }, 150);
+  }
 };
-button.addEventListener('click', handleLike.bind(this));
+button.addEventListener('click', handleClick.bind(this));
